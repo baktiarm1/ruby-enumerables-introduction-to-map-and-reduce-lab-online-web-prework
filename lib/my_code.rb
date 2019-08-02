9434bfc5b5cffdeef740155ab949be1af0bd305e
Baktiar Miah <baktiar.m@gmail.com>
2019-08-02 04:13:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,16 +39,6 @@ def reduce_to_total(array)
 return sum 
 end
 
-def reduce_to_any_true (array)
- i = 0
-  while i < array.length  do
-     if array[i] = true
-       i+=1
-       return true
-      end
-    end
-  end 
-  
  def reduce_to_all_true (array)
    val = true
    for x in array do 
